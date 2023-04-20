168686c02712ddfeb90919f66055810b5f2eb92a
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-04-20 14:56:31 +0100

    add og title, image and type

--- src/components/_App/Layout.jsx
+++ src/components/_App/Layout.jsx
@@ -13,9 +13,20 @@ const Layout = ({ children }) => {
         </title>
         <meta
           name="description"
-          content="The Emmanuel Eze Foundation empowers and transforms the lives of disadvantaged children and teens through quality education and skill acquisition training. Our free bootcamps prepare children for the demands of tomorrow's workplace."
+          content="ion
+Empowering children (especially disadvantaged ones) with cutting-edge tech skills for a better future."
         />
-        <meta property="og:image" content="/images/logo" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="ion
+Empowering children (especially disadvantaged ones) with cutting-edge tech skills for a better future."
+        />
+
+        <meta
+          property="og:image"
+          content="https://asset.cloudinary.com/dnyt1jfda/85ef88386fd38927e3265119b0d196b3"
+        />
         <meta
           name="keywords"
           content="Emmanuel Eze Foundation, EEF Bootcamps, EEF math bootcamp, EEF coding bootcamp, EEF design bootcamp, EEF, education, skill acquisition, disadvantaged children, bootcamps, coding, design, math, teens, workplace"
