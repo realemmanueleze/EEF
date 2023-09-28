1a836d649194b6c3c1a29308bafe22e321f36216
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-09-28 15:42:44 +0100

    update links

--- src/data/importantLinks.js
+++ src/data/importantLinks.js
@@ -1,9 +1,12 @@
 const importantLinks = {
   apply: "https://form.typeform.com/to/TXhYPqwa",
   donate: "https://donorbox.org/food-and-education-for-impoverished-children",
-  volunteer:
-    "https://docs.google.com/forms/d/e/1FAIpQLScoJmBGZrkBjdcX9U3EIhN1H2GgJAx1aDmaw1tco6K9-YkwiQ/viewform",
+  volunteer: "https://form.typeform.com/to/OQbkkXrF",
   discord: "https://discord.com/invite/7MK82Pp5VQ",
+  ndiEbonyiTechBootcamp: "https://form.typeform.com/to/eEBrh5nc",
+  ndiEbonyiTechBootcampWhatsApp:
+    "https://chat.whatsapp.com/CPXgKRNHwqk4CXT4ODDtx5 ",
+  volunteersWhatsAppGroup: "https://chat.whatsapp.com/BbeqtLC6LDgEaxHDa6i4Ka ",
 };
 
 export default importantLinks;
